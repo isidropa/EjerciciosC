76bc6429a5d8f0a8a4c51707dc2fdfdd74cdc37e
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-06 12:42:35 +0100

    Add files via upload

---
+++ 5.04.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Introduce un numero de tres digitos: ");
+    scanf("%d", &num);
+
+
+    printf("Digitos: %d", num / 100);
+    num = num % 100;
+    printf(" %d", num / 10);
+    num = num % 10;
+    printf(" %d", num);
+}
