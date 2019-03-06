2658659793ea6510f813819ac3d6161eb4799297
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-06 19:47:24 +0100

    Update 5.05.c

--- 5.05.c
+++ 5.05.c
@@ -20,6 +20,18 @@ void main()
             }
         }
     }
+    else if(x1 > x3)
+    {
+        aux = x1; x1 = x3; x3 = aux;
+        if(x2 > x3)
+        {
+           aux = x2; x2 = x3; x3 = aux;
+        }
+    }
+    else if(x2 > x3)
+    {
+        aux = x2; x2 = x3; x3 = aux;
+    }
 
     printf("\nx1 = %d, x2 = %d, x3 = %d", x1, x2, x3);
 }
