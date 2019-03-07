c6d6e4f625d00c3d584b450d93699a483f5d24e9
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-07 12:26:01 +0100

    Add files via upload

---
+++ 5.12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void main()
+{
+    int x1, x2, x3;
+    printf("x1 = "); scanf("%d", &x1);
+    printf("x2 = "); scanf("%d", &x2);
+    printf("x3 = "); scanf("%d", &x3);
+
+    if(x1 > x2 && x2 > x3)
+        printf("x3 < x2 < x1");
+    else if(x1 > x2 && x1 > x3)
+        printf("x2 < x3 < x1");
+    else if(x1 > x2 && x2 < x3 && x1 < x3)
+        printf("x2 < x1 < x3");
+    else if(x1 > x3)
+        printf("x3 < x1 < x2");
+    else if(x2 > x3)
+        printf("x1 < x3 < x2");
+    else
+        printf("x1 < x2 < x3");
+}
