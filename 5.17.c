71720987ab2e685c1d7cda401bb17cf79687f2a6
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-08 10:25:45 +0100

    Add files via upload

---
+++ 5.17.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+void main()
+{
+    float a, b, c, delta;
+    puts("\tCalculadora ecuaciones de 2 grado, \
+dame los valores de los coeficientes a, b y c");
+    printf("a b c: "); scanf("%f %f %f", &a, &b, &c);
+
+    if(a!=0 && b!=0 && c!=0)
+    {
+        delta = b*b - 4*a*c;
+        if(delta >= 0)
+            printf("x1 = %.4f\nx2 = %.4f",
+                   (-b + sqrt(delta)) / 2,
+                   (-b - sqrt(delta)) / 2);
+        else
+            printf("x1 = %.4f + %.4f i\nx2 = %.4f - %.4f i",
+                   -b/(2*a), sqrt(-delta/(4*a*a)),
+                   -b/(2*a), sqrt(-delta/(4*a*a)));
+    }
+    else if(a!=0 && b==0)
+    {
+        delta = -c/a;
+        if(delta >= 0)
+            printf("x1 = %.4f\nx2 = %.4f", sqrt(delta),
+                   -sqrt(delta));
+        else
+            printf("x1 = %.4f i\nx2 = %.4f i", sqrt(-delta),
+                   -sqrt(-delta));
+    }
+    else if(a==0 && b!=0)
+        printf("x = %.3f", -c/b);
+    else if(a==0 && b==0)
+        printf("a y b no pueden ser 0");
+}
