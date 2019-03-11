7d3d44c641dc28e01c87f7a7d01fc2f5c5f5b67e
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-11 14:19:21 +0100

    Add files via upload

---
+++ 6.9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    int a, b, cond=0;
+    puts("Fin de programa -> a>0, b>0 y b-a>100");
+
+    do
+    {
+        printf("a: "); scanf("%d", &a);
+        printf("b: "); scanf("%d", &b);
+        if(a>0 && b>0 && b-a>100)
+            cond = -1;
+        else
+            printf("\n");
+    }while(cond != -1);
+}
