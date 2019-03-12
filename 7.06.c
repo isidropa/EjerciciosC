28dc00ca13c9559329bdf382c3e0ab5e3aa67b87
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-12 18:45:37 +0100

    Add files via upload

---
+++ 7.06.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+short esPrimoPerfecto(short);
+short esPrimo(short);
+void main()
+{
+    puts("\tNumeros primos perfectos menores que 1000\n");
+    for(short i=1; i<1000; i++)
+        if(esPrimoPerfecto(i)) printf("%3hd\n", i);
+}
+
+short esPrimoPerfecto(short n)
+{
+    if(esPrimo(n))
+    {
+        if(esPrimo((n-1) / 2))  return 1;
+        else                    return 0;
+    }
+    else                        return 0;
+}
+short esPrimo(short n)
+{
+    short cont=0;
+    for(short i=1; i<=n; i++)
+        if(n%i == 0) cont++;
+    if(cont > 2)                return 0;
+    else if(cont == 2)          return 1;
+    else                        return 0;
+}
