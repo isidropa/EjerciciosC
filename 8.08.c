b56b3522a6e82acad9d749911b20ef8bea69844e
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-14 20:34:31 +0100

    Add files via upload

---
+++ 8.08.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void intercambio(int *ant, int* act)
+{
+    int aux = *act;
+    *act = *ant;
+    *ant = aux;
+}
+void rotarVectorDer(int v[], short tam, short num)
+{
+    for(int j=1; j<=num; j++)
+        for(int i=0; i<tam; i++)
+            intercambio(&v[tam-1], &v[i]);
+}
+
+void main()
+{
+    int vector[5] = {0, 1, 2, 3, 4};
+    rotarVectorDer(vector, 5, 2);
+    for(short i=0; i<5; i++)
+        printf("%d\t", vector[i]);
+}
