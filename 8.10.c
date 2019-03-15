43a4fb3e4ec32e788908b0a9563b75fd6d0aa164
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-15 19:16:26 +0100

    Add files via upload

---
+++ 8.10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+    srand(time(NULL));
+    int v[20], v2[20][2]={0};
+    short si=0, c=0;
+
+    printf("\tAgrupador de valores aleatorios\nValores: ");
+    for(int i=0; i<20; i++){
+        v[i] = rand() % 25 + 1;
+        printf("%d ", v[i]);
+    }
+    for(int i=0; i<20; i++){
+        for(int j=0; j<20; j++)
+            if(v2[j][0] == v[i]){
+                v2[j][1]++;
+                si = 1;
+            }
+        if(si)
+            si=0;
+        else{
+            v2[c][0] = v[i];
+            v2[c][1]++;
+            c++;
+        }
+    }
+    printf("\nValores repetidos: ");
+    for(int i=0; v2[i][0] > 0; i++)
+        if(v2[i][1] > 1)
+            printf("\n %2d -> %2d veces", v2[i][0], v2[i][1]);
+}
