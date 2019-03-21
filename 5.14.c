0c98fb5d0fa8a76c1a2e35f69643c53c1279c403
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-21 18:39:00 +0100

    Update 5.14.c

--- 5.14.c
+++ 5.14.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main()
 {
-    short px, py, cx, cy, r;
+    long px, py, cx, cy, r;
     printf("Punto:\nP_x: "); scanf("%hd", &px);
     printf("P_y: "); scanf("%hd", &py);
     printf("\nCentro:\nc_x: "); scanf("%hd", &cx);
     printf("c_y: "); scanf("%hd", &cy);
     printf("\nRadio:\t"); scanf("%hd", &r);
 
-    short pmod_x = px - cx, pmod_y = py - cy;
+    long pmod_x = px - cx, pmod_y = py - cy;
 
     if( (pmod_x == r && pmod_y == 0)  ||
         (pmod_x == 0 && pmod_y == r)  ||
