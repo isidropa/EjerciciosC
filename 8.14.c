001439ff5867453fe877cb656ca008f5706e786f
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 13:01:38 +0100

    Update 8.14.c

--- 8.14.c
+++ 8.14.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 int sumaValPeriMatriz(int v[3][4], int n, int m)
 {
-    int suma=0;
-    short i,j;
-    i=0;
-    for(j=0; j<m; j++)
-        suma += v[i][j];
-    i=n-1;
-    for(j=0; j<m; j++)
-        suma += v[i][j];
-    j=0;
-    for(i=1; i<n-1; i++)
-        suma += v[i][j];
-    j=m-1;
-    for(i=1; i<n-1; i++)
-        suma += v[i][j];
+    int i,j, suma=0;
+    i=0;    for(j=0; j<m; j++)
+                suma += v[i][j];
+    i=n-1;  for(j=0; j<m; j++)
+                suma += v[i][j];
+    j=0;    for(i=1; i<n-1; i++)
+                suma += v[i][j];
+    j=m-1;  for(i=1; i<n-1; i++)
+                suma += v[i][j];
     return suma;
 }
 void impMatriz(int v[3][4], int n, int m)
 {
-    for(short i=0; i<n; i++)
+    for(int i=0; i<n; i++)
     {
-        for(short j=0; j<m; j++)
+        for(int j=0; j<m; j++)
             printf(" %2d", v[i][j]);
         puts("");
     }
@@ -30,7 +25,7 @@ void main()
 {
     printf("\tLa suma de los valores de la periferia debe ");
     printf("superar a cualquier otro valor de la matriz");
-    short n=3, m=4;
+    int n=3, m=4;
     int periferia, aux, matriz[3][4]= { {1,  3,   6, 1},
                                         {2, 41,  80, 6},
                                         {2,  7,   9, 3}  };
@@ -40,8 +35,8 @@ void main()
     periferia = sumaValPeriMatriz(matriz, n, m);
     printf("Suma periferia: %d\n", periferia);
     //Comprobar y modificar
-    for(short i=1; i<n-1; i++)
-        for(short j=1; j<m-1; j++)
+    for(int i=1; i<n-1; i++)
+        for(int j=1; j<m-1; j++)
             if(matriz[i][j] > periferia)
             {
                 aux = matriz[i][j];
