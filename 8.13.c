1b28b334a79d229176597bb4869adcc5cd08e25a
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 12:54:57 +0100

    Update 8.13.c

--- 8.13.c
+++ 8.13.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 void main()
 {
-    short m2[3][4], m1[3][4] = { {1, 2, 7, 7},
-                                 {8, 4, 5, 5},
-                                 {6, 3, 8, 6}  };
-    short cF=0,cC=0;
+    long m2[3][4], m1[3][4] = { {1, 2, 7, 7},
+                                {8, 4, 5, 5},
+                                {6, 3, 8, 6}  };
+    long cF=0,cC=0;
     //Introducir valores pares de m1 en m2
-    for(short i=0; i<3; i++)
-        for(short j=0; j<4; j++)
+    for(long i=0; i<3; i++)
+        for(long j=0; j<4; j++)
             if(m1[i][j]%2 == 0)
             {
                 m2[cF][cC++] = m1[i][j];
@@ -18,8 +18,8 @@ void main()
                 }
             }
     //Introducir valores impares de m1 en m2
-    for(short i=0; i<3; i++)
-        for(short j=0; j<4; j++)
+    for(long i=0; i<3; i++)
+        for(long j=0; j<4; j++)
             if(m1[i][j]%2 > 0)
             {
                 m2[cF][cC++] = m1[i][j];
@@ -30,10 +30,10 @@ void main()
                 }
             }
     //Imprimir m2
-    for(short i=0; i<3; i++)
+    for(long i=0; i<3; i++)
         {
-        for(short j=0; j<4; j++)
-            printf(" %hd", m2[i][j]);
+        for(long j=0; j<4; j++)
+            printf(" %d", m2[i][j]);
         printf("\n");
         }
 }
