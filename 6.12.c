4dd8943091379306171b7f617c537da5e2c8193e
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 10:57:47 +0100

    Update 6.12.c

--- 6.12.c
+++ 6.12.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void main()
 {
-    for(int a=1; a<51; a++)
-        for(int b=1; b<51; b++)
-            for(int c=1; c<51; c++)
+    for(long a=1; a<51; a++)
+        for(long b=1; b<51; b++)
+            for(long c=1; c<51; c++)
             {
                 if(a*a + b*b == c*c)
                     printf("(%d, %d, %d)\n", a, b, c);
