6a34562b082e3bca3c9349c28e1f914ff136630a
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 12:20:27 +0100

    Update 8.07.c

--- 8.07.c
+++ 8.07.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-short primo(int n)
+short primo(long n)
 {
     short c=0;
-    for(int i=1; i<=n; i++)
+    for(long i=1; i<=n; i++)
         if(n%i == 0)
             c++;
     if(c == 2)  return 1;
     else        return 0;
 }
-short todosPrimos(int v[], short tam)
+short todosPrimos(long v[], long tam)
 {
     short c=0;
-    for(short i=0; i<tam; i++)
+    for(long i=0; i<tam; i++)
         if(primo(v[i]))
             c++;
     if(c == tam)return 1;
@@ -20,7 +20,7 @@ short todosPrimos(int v[], short tam)
 
 void main()
 {
-    int v1[3] = {3, 5, 11}, v2[3] = {1, 2, 3};
+    long v1[3] = {3, 5, 11}, v2[3] = {1, 2, 3};
     puts("\t1=Todos son primos\t0=Alguno no es primo");
     printf("v1[]: %d, %d, %d\n", v1[0], v1[1], v1[2]);
     printf("v2[]: %d, %d, %d\n", v2[0], v2[1], v2[2]);
