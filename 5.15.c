ff0e3145ca0fb24cc5218882b506477fef7c5cc0
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 09:59:06 +0100

    Update 5.15.c

--- 5.15.c
+++ 5.15.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 void main()
 {
-    float px1, py1, px2, py2, m, n;
+    double px1, py1, px2, py2, m, n;
     printf("Puntos:\nP1_x: "); scanf("%f", &px1);
-    printf("P1_y: "); scanf("%f", &py1);
-    printf("\nP2_x: "); scanf("%f", &px2);
-    printf("P2_y: "); scanf("%f", &py2);
+    printf("P1_y: "); scanf("%lf", &py1);
+    printf("\nP2_x: "); scanf("%lf", &px2);
+    printf("P2_y: "); scanf("%lf", &py2);
 
     m = (py1-py2)/(px1-px2);
     n = py1 - m * px1;
 
-    printf("\ny = %.2f x * %.2f", m, n);
+    printf("\ny = %.2f x + %.2f", m, n);
 }
