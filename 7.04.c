2dd31a0d3adb701723f05f2546b52191f493c33e
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 11:23:59 +0100

    Update 7.04.c

--- 7.04.c
+++ 7.04.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 //Declaracion del tipo bool manualmente
 enum bool {False, True};
-enum bool esPerfecto(unsigned long);
+enum bool esPerfecto(long);
 void main()
 {
-    unsigned long a, b;
+    long a, b;
     puts("Numeros perfectos en el intervalo [a, b]");
-    printf("a: "); scanf("%lu", &a);
-    printf("b: "); scanf("%lu", &b);
-    for(unsigned long i=a; a<b; a++)
+    printf("a: "); scanf("%d", &a);
+    printf("b: "); scanf("%d", &b);
+    for(long i=a; a<b; a++)
         if(esPerfecto(a))
-            printf("%lu\n", a);
+            printf("%d\n", a);
 }
 
-enum bool esPerfecto(unsigned long n)
+enum bool esPerfecto(long n)
 {
-    unsigned long suma=0;
+    long suma=0;
     enum bool bool1;
-    for(unsigned long i=1; i<n; i++)
+    for(long i=1; i<n; i++)
         if(n%i == 0)
             suma += i;
     if(suma == n)
