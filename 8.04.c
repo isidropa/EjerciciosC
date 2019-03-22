acb1781ba12eda2ff7006b81d7829b2e729a79ee
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-22 12:05:20 +0100

    Update 8.04.c

--- 8.04.c
+++ 8.04.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-int ind_mayor(int v[], short);
+long ind_mayor(long v[], long);
 void main()
 {
-    int aleatorios[100], pos;
+    long aleatorios[100], pos;
     srand(time(NULL));
     puts("100 numeros aleatorios entre 0 y 100:\n");
 
-    for(short i=0; i<100; i++)
+    for(long i=0; i<100; i++)
     {
         aleatorios[i] = rand() % 101;
-        printf("%3hd\t", aleatorios[i]);
+        printf("%3d\t", aleatorios[i]);
     }
     pos = ind_mayor(aleatorios, 100);
     printf("\n\nv[%d]: %d", pos, aleatorios[pos]);
 }
 
-int ind_mayor(int v[], short n)
+long ind_mayor(long v[], long n)
 {
-    int mayor = v[0], indice=0;
-    for(short i=1; i<n; i++)
+    long mayor = v[0], indice=0;
+    for(long i=1; i<n; i++)
         if(mayor < v[i]) { mayor = v[i]; indice = i; }
     return indice;
 }
