ddc98e71010bc0c6d03d77cb7ae78288860ab9bd
isidropa <36481520+isidropa@users.noreply.github.com>
2019-03-25 10:24:12 +0100

    Update 7.06.c

--- 7.06.c
+++ 7.06.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
-short esPrimo(short);
+long esPrimo(long);
 void main()
 {
     puts("\tNumeros primos perfectos menores que 1000\n");
-    for(short n=1; n<1000; n++)
+    for(long n=1; n<1000; n++)
         if(esPrimo(n) && esPrimo((n-1)/2))
-           printf("%3hd\n", n);
+           printf("%3d\n", n);
 }
 
-short esPrimo(short n)
+long esPrimo(long n)
 {
-    short cont=0;
-    for(short i=1; i<=n; i++)
+    long cont=0;
+    for(long i=1; i<=n; i++)
         if(n%i == 0) cont++;
-    if(cont > 2)                return 0;
-    else if(cont == 2)          return 1;
-    else                        return 0;
+    if(cont == 2)   return 1;
+    else            return 0;
 }
